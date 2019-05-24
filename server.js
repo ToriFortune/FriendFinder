c9db7c5f6c218ce7e57a2afdda05c644c42f351d
Tori Fortune <toriolley@gmail.com>
2019-05-24 00:24:43 -0500

    created friends array, included app.listen function to test connection in friends. js file, created express, app and port variables and tested connection in server.js file

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var express = require("");
-var connection = require ("./db/connection");
-var path = require ("path");
+const express = require("express");
+const app = express ();
+const PORT = process.env.PORT ||3000;
 
-var app =express();
+// var path = require ("path");
 
-var port = process.env.PORT || 3000;
+// var app =express();
+
+// var port = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+    console.log ("We are in!")
+  });
