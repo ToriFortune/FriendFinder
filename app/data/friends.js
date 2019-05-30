db8c494fb84e1138a9c3f553f773188216a1b9a1
Tori Fortune <toriolley@gmail.com>
2019-05-29 22:43:02 -0500

    working on connecting files,

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,26 @@
 
-const friends =[{
+const friends = [{
     name: "Boris",
-    photo:"https://www.tvovermind.com/wp-content/uploads/2017/07/Boris-Kodjoe-640x360.jpg",
-    email:"boris@bemyfriend.com",
-    id:"Big Bor",
-    phoneNumber: "202-121-1250",
-    scores: ["5","6", "5", "5", "4"]
+    photo: "https://www.tvovermind.com/wp-content/uploads/2017/07/Boris-Kodjoe-640x360.jpg",
+    scores: ["5", "6", "5", "5", "4"]
 },
 {
     name: "Nicole",
     photo: "https://pbs.twimg.com/profile_images/522077042347483136/NeoXpqvT.jpeg",
-    email:"nikki@bemyfriend.com",
-    id:"Gorg Nikki",
-    phoneNumber: "203-121-1250",
-    scores: ["8","8", "8", "7", "7"]
+    scores: ["8", "8", "8", "7", "7"]
 },
 {
     name: "Nigel",
     photo: "http://totallyhistory.com/wp-content/uploads/2013/06/nigel-barker-5.jpg",
-    email:"nigel@bemyfriend.com",
-    id:"Nice Nigel",
-    phoneNumber: "206-121-1250",
-    scores: ["6","5", "4", "7", "3"]
+    scores: ["6", "5", "4", "7", "3"]
 },
 {
     name: "Jason",
     photo: "https://i.pinimg.com/originals/bb/61/4a/bb614a9ea42c10ba84fb13e665545534.jpg",
-    email:"Jason@bemyfriend.com",
-    id:"Buff Jay",
-    phoneNumber: "202-121-5550",
-    scores: ["2","5", "1", "5", "5"]
+    scores: ["2", "5", "1", "5", "5"]
 }];
 
 
 //   making friends a dependency to be required in necessary files by exporting.
-  module.exports =friends;
+module.exports = friends;
+console.log("???????")
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 
 // Dependencies to be imported or exported
 
-const friends = require ("../data/friends.js");
+const friends = require("../data/friends.js");
 const path = require("path");
 
 
@@ -10,25 +10,51 @@ const path = require("path");
 module.exports = (app) => {
 
   // All possible friends
-//   app.get("/api/friends", (req, res) => {
-//     res.send(friends);
-//   })
-  app.get("/api/friends", (req, res) => {
+  //   app.get("/api/friends", (req, res) => {
+  //     res.send(friends);
+  //   })
+  app.get("/api/friends", (req, res, err) => {
     res.json(friends);
   })
 
   // Post survery input
-  app.post("/api/friends", (req, res,) => {
-        // store user data
-        const newUser = {
-            name: req.body.name,
-            photo: req.body.photo,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            scores: req.body.scores
-        };
+  app.post("/api/friends", (req, res, err ) => {
+    // store user data
+    const newUser = {
+      name: req.body.name,
+      photo: req.body.photo,
+     scores: req.body.scores
+    };
+    //compare new user's scores to all existing users' scores
+    //to determine the best match
+  });
+}
 
-        //compare new user's scores to all existing users' scores
-        //to determine the best match
-    });
-}   
+const bestMatch ={
+  name:"",
+  photo:"",
+  diff: null,
+}
+
+for (var i=0; i<friends.length;){
+  var currentUser =friends [i]
+  var userDifference=0;
+  for (var j=0; j<currentUser.scores.length; j++){userDifference+=Math.abs(parseInt(currentUser.scores[j]))
+    
+  }
+  if (userDifference<bestMatch.diff){
+    bestMatch.name=currentUser.name;
+    bestMatch.photo=currentUser.photo;
+    bestMatch.diff=currentUser.diff;
+    console.log("FOUND: " + bestMatch)
+    break;
+  } else {
+    console.log("Not a match.")
+    bestMatch = "No match!"
+  }
+}
+
+friends.push(newFriend);
+res.json(bestMatch);
+
+
