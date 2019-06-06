e15ea87699db83604f9d5bbc766143b6dd8e426a
Tori Fortune <toriolley@gmail.com>
2019-06-05 21:38:21 -0500

    created procfile

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -15,7 +15,7 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("/home", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
